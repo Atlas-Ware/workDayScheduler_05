b96f04757c3c0bc6f6d1fa48d983a2cc65b796d9
Ignacio Albistu <profiles.albistu@gmail.com>
2022-12-13 11:34:30 -0500

    reworked code 1st attempt

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,13 @@
 // Today's date, time & day
 var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
+$("#today").html(todayDate);
 
 
 $(document).ready(function () {
 // This 1st part of the function saves any data typed in each time block
-    $(".saveBtn").on("click", function () {
-// This varaibale saves the text that is type in
-        var text = $(this).siblings(".description").val();
+    $("button").on("click", function () {
+// This varaibale saves the text that is typed in
+        var text = $(this).siblings("textarea").val();
 // This varaibale saves my ID of the specific time where the text was written
         var time = $(this).parent().attr("id");
 // This takes var text and time and stores it in local storage
@@ -19,8 +19,8 @@ $(document).ready(function () {
         var timeNow = moment().hour();
 
 // Time blocks loops
-        $(".time-block").each(function () {
-// This varaiable are the specific time blocks that were made in the HTML
+        $(".time").each(function () {
+// This varaiable is the specific time blocks that were made in the HTML
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
             //console.log(blockTime);
 
@@ -45,15 +45,15 @@ $(document).ready(function () {
     }
 
 // Returns info that was typed into local storage and keeps it, even if browser is refreshed
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour9 textarea").val(localStorage.getItem("hour9"));
+    $("#hour10 textarea").val(localStorage.getItem("hour10"));
+    $("#hour11 textarea").val(localStorage.getItem("hour11"));
+    $("#hour12 textarea").val(localStorage.getItem("hour12"));
+    $("#hour13 textarea").val(localStorage.getItem("hour13"));
+    $("#hour14 textarea").val(localStorage.getItem("hour14"));
+    $("#hour15 textarea").val(localStorage.getItem("hour15"));
+    $("#hour16 textarea").val(localStorage.getItem("hour16"));
+    $("#hour17 textarea").val(localStorage.getItem("hour17"));
 
     timeTracker();
 })
